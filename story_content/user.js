2a652a6da4bc14ae39232fd439fa0d54e9fdb8ae
Cakra Penta <cakra.penta.0@gmail.com>
2025-08-04 09:38:33 +0700

    upload

---
+++ story_content/user.js
@@ -0,0 +1,20 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5fRs8ovXuCw":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+audio.volume=0.2;
+
+}
+
